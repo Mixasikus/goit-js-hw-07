088caef98939b80db35ad439f2e1af7716a8e54b
Mixasikus <mixasiikus@gmail.com>
2022-06-02 00:04:59 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryListEl = document.querySelector(".gallery");
+const galleryItemEl = galleryItems.map(({ preview, original, description }) => `<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      srcset="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`).join("");
+
+galleryListEl.insertAdjacentHTML("beforeend", galleryItemEl);
+
+
+const img = document.querySelector('img');
+console.log(img);
+
+galleryListEl.addEventListener("click", (event) => {
+    event.preventDefault();
+    
+    console.log(event.target);
+});
+
+
+// console.log(img.getAttribute("data-source"));
+
+
+
+
+// const img = document.querySelector('img[data-source]')
+
+
